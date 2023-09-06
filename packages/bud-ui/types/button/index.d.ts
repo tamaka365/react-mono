e24844cb0d68e3fa42bad5da603789d398fb0c03
tamaka365 <tamaka365@gmail.com>
2023-09-06 16:30:37 +0800

    perf

--- packages/bud-ui/types/button/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface ButtonProps {
-    children?: React.ReactNode;
-    onClick?(): void;
-}
-declare const Button: React.FC<ButtonProps>;
-export default Button;
--- packages/bud-ui/types/input/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface InputProps {
-    children?: React.ReactNode;
-}
-declare const Input: React.FC<InputProps>;
-export default Input;
