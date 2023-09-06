4d2892120c19be04f94261fbfedd1134994baef2
tamaka365 <tamaka365@gmail.com>
2023-09-06 20:01:01 +0800

    perf

--- packages/bud-ui/vite.config.ts
+++ packages/bud-ui/vite.config.ts
@@ -1,25 +1,33 @@
-import { defineConfig, splitVendorChunkPlugin } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    // outDir: './dist',
     lib: {
       entry: path.resolve(__dirname, 'src/lib/index.ts'),
-      name: 'bud-ui',
-      fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
+      external: ['react'],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          exports: 'named',
+          // dir: './dist',
         },
-      },
+        {
+          format: 'cjs',
+          entryFileNames: '[name].cjs',
+          preserveModules: true,
+          exports: 'named',
+          // dir: './dist',
+        },
+      ],
     },
     copyPublicDir: false,
   },
-  plugins: [react(), splitVendorChunkPlugin()],
 });
